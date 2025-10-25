d3800befa0e6868a61303369a484302ce1b7ff9f
dcowett <david@davlinalli.com>
2025-10-25 18:43:01 -0400

    add csv export of notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,6 +4,12 @@ class NotesController < ApplicationController
   # GET /notes/
   def index
     @notes = Note.all.order("created_at DESC")
+
+    respond_to do |format|
+      format.html
+      #format.csv { render text: @notes.to_csv }
+      format.csv { send_data Note.to_csv, filename: "notes-#{DateTime.now.strftime("%d%m%Y%H%M")}.csv"}
+    end
   end
 
   private
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,12 @@
 class Note < ApplicationRecord
   belongs_to :property
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |note|
+        csv << note.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
