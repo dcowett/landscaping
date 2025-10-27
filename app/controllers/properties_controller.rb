abca3476c3eee6f3889953264106678748a27d93
dcowett <david@davlinalli.com>
2025-10-27 05:21:06 -0400

    removed notes views and added back link

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -84,7 +84,7 @@ class PropertiesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_property
-      @property = Property.find(params.expect(:id))
+      @property = Property.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
