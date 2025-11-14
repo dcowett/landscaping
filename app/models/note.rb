18550fbc2e87cd2e1084c3f46dfb462cf042ca6a
dcowett <david@davlinalli.com>
2025-11-13 19:57:27 -0500

    fix notes export timeout

--- app/models/note.rb
+++ app/models/note.rb
@@ -9,13 +9,13 @@ class Note < ApplicationRecord
   end
 
   def self.to_csv
+    propertyArray = Property.pluck(:id, :situs_address)
     CSV.generate do |csv|
-      column_names = %w[ property_id situs_address updated_at created_at code note ]
+      column_names = %w[ id property_id situs_address updated_at created_at code note ]
           csv << column_names
           all.each do |note|
-            @property = Property.find(note.property_id)
-            @address = @property.situs_address
-            csv << [ note.property_id, @address, note.updated_at, note.created_at, note.code, note.notes  ]
+            @address = propertyArray.find { |id| id[0] == note.property_id }
+            csv << [ note.id, note.property_id, @address, note.updated_at, note.created_at, note.code, note.notes  ]
       end
     end
   end
