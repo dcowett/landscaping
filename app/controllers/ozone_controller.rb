fb2c9d5fcadd99eb588ae5ff42233c6625f2f9b6
dcowett <david@davlinalli.com>
2025-11-17 20:04:43 -0500

    fix ozone background colors

--- app/controllers/ozone_controller.rb
+++ app/controllers/ozone_controller.rb
@@ -20,27 +20,27 @@ def index
     end
 
     if @final_output == "Error"
-      @api_color = "grey"
+      @api_color = "bg-silver"
     elsif @final_output <= 50
-      @api_color = "green"
+      @api_color = "bg-green"
       @api_description="Air quality is satisfactory, and air pollution poses little or no risk."
     elsif @final_output >= 51 && @final_output <= 100
-      @api_color = "yellow"
+      @api_color = "bg-yellow"
       @api_description="Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution."
     elsif @final_output >= 101 && @final_output <= 150
-      @api_color = "orange"
+      @api_color = "bg-orange"
       @api_description="	Members of sensitive groups may experience health effects. The general public is less likely to be affected."
     elsif @final_output >= 151 && @final_output <= 200
-      @api_color = "red"
+      @api_color = "bg-red"
       @api_description="	Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects."
     elsif @final_output >= 201 && @final_output <= 250
-      @api_color = "purple"
+      @api_color = "bg-purple"
       @api_description="Health alert: The risk of health effects is increased for everyone."
     elsif @final_output >= 251 && @final_output <= 500
-      @api_color = "maroon"
+      @api_color = "bg-maroon"
       @api_description="Health warning of emergency conditions: everyone is more likely to be affected."
     else
-      @api_color = "grey"
+      @api_color = "bg-silver"
     end
   end
 
@@ -71,27 +71,27 @@ def index
     end
 
     if @final_output == "Error"
-      @api_color = "grey"
+      @api_color = "bg-silver"
     elsif @final_output <= 50
-      @api_color = "green"
+      @api_color = "bg-green"
       @api_description="Air quality is satisfactory, and air pollution poses little or no risk."
     elsif @final_output >= 51 && @final_output <= 100
-      @api_color = "yellow"
+      @api_color = "bg-yellow"
       @api_description="Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution."
     elsif @final_output >= 101 && @final_output <= 150
-      @api_color = "orange"
+      @api_color = "bg-orange"
       @api_description="	Members of sensitive groups may experience health effects. The general public is less likely to be affected."
     elsif @final_output >= 151 && @final_output <= 200
-      @api_color = "red"
+      @api_color = "bg-red"
       @api_description="	Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects."
     elsif @final_output >= 201 && @final_output <= 250
-      @api_color = "purple"
+      @api_color = "bg-purple"
       @api_description="Health alert: The risk of health effects is increased for everyone."
     elsif @final_output >= 251 && @final_output <= 500
-      @api_color = "maroon"
+      @api_color = "bg-maroon"
       @api_description="Health warning of emergency conditions: everyone is more likely to be affected."
     else
-      @api_color = "grey"
+      @api_color = "bg-silver"
     end
     end
 
