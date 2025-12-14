d1f775916bcd0f172be9b7ee095869f514837247
dcowett <david@davlinalli.com>
2025-12-14 17:51:04 -0500

    fixed comma separation in edit stories

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   acts_as_tagger
-  acts_as_taggable_on :tags
-  acts_as_taggable_on :stories
+  acts_as_taggable_on :tags, :stories
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,8 @@ Rails.application.routes.draw do
   resources :pins
 
   devise_for :users, controllers: {
-        sessions: 'users/sessions',
-        registrations: 'users/registrations'
+        sessions: "users/sessions",
+        registrations: "users/registrations"
       }
 
   devise_scope :user do
