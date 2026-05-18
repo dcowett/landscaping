53b1c6d80a37c335cfd6fe56d43e01c8c2b0d8af
dcowett <david@davlinalli.com>
2026-05-18 19:51:45 -0400

    change favicon and refactor application

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,5 @@ class Vote < ApplicationRecord
   belongs_to :story, counter_cache: true
   belongs_to :user
   validates :story_id, presence: true
+  validates :user_id, uniqueness: { scope: :story_id }
 end
